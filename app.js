773988ba51cf8052a4cda1d963874c9e98290a9d
Hasibul Hasan <hasanhasibul395@gmail.com>
2022-06-04 02:57:58 +0600

    trying

--- app.js
+++ app.js
@@ -23,8 +23,12 @@ const limiter = rateLimit({
 })
 
 // middleware implement 
-
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(mongoSanitize());
